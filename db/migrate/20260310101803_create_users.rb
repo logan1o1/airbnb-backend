b72174f6edeefb9625efce2345254a9e14d762c4
logan1o1 <saswatm2706@gmail.com>
2026-03-10 16:42:01 +0530

    Project setup and one migration run

---
+++ db/migrate/20260310101803_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name, limit: 50
+      t.string :last_name, limit: 50
+      t.string :username 
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
